3f3d6f8db83ff2714b940a0dde2f7143d7916845
Viacheslav Bobrov <babrovka@gmail.com>
2013-08-23 13:06:08 +0400

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,39 @@
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+load 'deploy/assets'
+
+server "5.178.80.26", :web, :app, :db, primary: true
+
+set :user, "user"
+set :application, "ncp"
+set :deploy_to, "/home/user/projects/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:babrovka/ncp.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+
+
+
+namespace(:thin) do
+  task :stop do
+    run "thin stop -C /etc/thin/kzs.yml"
+   end
+  
+  task :start do
+    run "thin start -C /etc/thin/kzs.yml"
+  end
+
+  task :restart do
+    run "thin restart -C /etc/thin/kzs.yml"
+  end
+end
+
+
+after "deploy", "deploy:cleanup"
