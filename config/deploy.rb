f5862229334a411c6c21883626b5a12c8933d6a2
Viacheslav Bobrov <babrovka@gmail.com>
2013-08-23 14:45:36 +0400

    project

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,18 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
 
 
 namespace(:thin) do
