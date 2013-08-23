b1ea8a3b54291507fc74a30b8ca8e604411eef22
Viacheslav Bobrov <babrovka@gmail.com>
2013-08-23 14:48:53 +0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,11 @@ namespace :deploy do
   end
 end
 
+task :copy_database_config do
+   db_config = "#{shared_path}/development.sqlite3"
+   run "cp #{db_config} #{latest_release}/db/development.sqlite3"
+end
+
 
 namespace(:thin) do
   task :stop do
